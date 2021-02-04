f679560aff974f1764f7961aef83070e9072987d
Joel Chenoweth <42840168+jchenwth101@users.noreply.github.com>
2021-02-03 18:58:50 -0600

    Update tests.py

--- tests.py
+++ tests.py
@@ -5,11 +5,12 @@ from credit_card_validator import credit_card_validator
 
 
 class TestCredCardNumber(TestCase):
-    def test_random_length16(self):
-        for x in range(0, 100):
-            random_val = random.randint(1000000000000000, 9999999999999999)
+    for x in range(0, 1000):
+        for i in range(2221, 2721):
+            cc_prefix = [int(d) for d in str(i)]
+            random_val = random.randint(100000000000, 999999999999)
             subs_card = [int(d) for d in str(random_val)]
-            cc_list = subs_card
+            cc_list = cc_prefix + subs_card
             cc_list = [str(i) for i in cc_list]
             cc_num = int("".join(cc_list))
             credit_card_validator(str(cc_num))
