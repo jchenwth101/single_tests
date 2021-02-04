060c3a3a9559bcf8092d8eaa11f90a1838dd2720
Joel Chenoweth <42840168+jchenwth101@users.noreply.github.com>
2021-02-03 18:37:13 -0600

    Update tests.py

--- tests.py
+++ tests.py
@@ -13,12 +13,6 @@ class TestCredCardNumber(TestCase):
             cc_list = [str(i) for i in cc_list]
             cc_num = int("".join(cc_list))
             credit_card_validator(str(cc_num))
-
-    def test_random_length1_length16(self):
-        for x in range(0, 1000):
-            random_val = random.randint(1, 9999999999999999)
-            random_num_list = [int(d) for d in str(random_val)]
-            cc_list = [str(i) for i in random_num_list]
-            cc_num = int("".join(cc_list))
-            credit_card_validator(str(cc_num))
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
 
